fd32effecc3f265bb36a0246bca3013beb2621cd
Ceyvion Biggs <ceyvionb@gmail.com>
2019-12-12 23:36:59 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -12,10 +12,13 @@ end
 def not_safe?(speed)
 	if speed > 60
 	  return true
-	else 
+	elsif  
 	  speed < 40
-	  return true 
-  end
+	  return true
+	else 
+	  speed.between(40..60)
+	  return false 
+ end
 end 
 
 
